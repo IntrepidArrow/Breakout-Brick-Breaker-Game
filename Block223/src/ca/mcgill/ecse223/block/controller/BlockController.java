2976b384d73926c3d80555ce32680294539d4a35
Albert Assouad <albert.assouad@mail.mcgill.ca>
2019-02-09 19:21:38 -0500

    --

--- Block223/src/ca/mcgill/ecse223/block/controller/BlockController.java
+++ Block223/src/ca/mcgill/ecse223/block/controller/BlockController.java
@@ -4,11 +4,18 @@ import ca.mcgill.ecse223.block.model.Block;
 import ca.mcgill.ecse223.block.model.Block223;
 import ca.mcgill.ecse223.block.model.Game;
 import ca.mcgill.ecse223.block.model.Level;
+import ca.mcgill.ecse223.block.application.BlockApplication;;
 
 public class BlockController {
 	
-	public static void positionBlock(int id, int xPos , int yPos) {
-		Block223 block223;
+	// block is already created, just position it
+	public static void positionBlock(int id, int xPos , int yPos,String gameName) {
+		Block223 block223 = BlockApplication.getBlockApp();
+		/*conditions: number of blocks less than the limit per level
+		 * check if there is already a block assignment at this pos
+		 * create a blockAssignment and add it to a level
+		 * block.addBlockAssignment(xpos,ypos,level,game)
+		 */
 		
 		// get game, get level
 		//level.addBlockAssignment(int aGridHorizontalPosition, int aGridVerticalPosition, Block aBlock, Game aGame);
@@ -20,27 +27,16 @@ public class BlockController {
 		
 	}
 	
-	private static Block findBlock(int idBlock, String idGame) {
-		Block foundBlock = null;
-		Game game = findGame(idGame);
-		for (Block block : game.getBlocks() ) {
-			if (block.getId() == idBlock) {
-				foundBlock = block;
-				break;
-			}
-		}
-		return foundBlock;
-	}
-	
-	private static Game findGame(String idGame) {
-		Game foundGame = null;
-		for(Game game: block223.getGames) {
-			if(game.getName() == idGame) {
-				foundGame = game;
-				break;
-			}
-		}
-		return foundGame;
-		
-	}
+	/*
+	 * private static Block findBlock(int idBlock, String idGame) { Block foundBlock
+	 * = null; Game game = findGame(idGame); for (Block block : game.getBlocks() ) {
+	 * if (block.getId() == idBlock) { foundBlock = block; break; } } return
+	 * foundBlock; }
+	 * 
+	 * private static Game findGame(String idGame) { Game foundGame = null; for(Game
+	 * game: block223.getGames) { if(game.getName() == idGame) { foundGame = game;
+	 * break; } } return foundGame;
+	 * 
+	 * }
+	 */
 }
