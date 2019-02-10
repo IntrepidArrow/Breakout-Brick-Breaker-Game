3856777ba6855096058927dc266d9334aed883a0
Abhimukth Chaudhuri <abhimukth.chaudhuri@mail.mcgill.ca>
2019-02-09 19:48:26 -0500

    Added comment to return statement
    Added a comment to prevent any confusion about what the method will return depending on the result of the if statement

--- Block223/src/ca/mcgill/ecse223/block/application/BlockApplication.java
+++ Block223/src/ca/mcgill/ecse223/block/application/BlockApplication.java
@@ -11,6 +11,7 @@ public class BlockApplication {
 		if (blockApp == null) {
 			blockApp = new Block223();
 		}
+		//returns existing an existing blockApplication if there exists a block application prior to check 
  		return blockApp;
 	}
 }
