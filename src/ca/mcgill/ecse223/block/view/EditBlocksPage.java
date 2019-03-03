77ffec40d9c3f4fc18f75e1eb34e3566d07dc383
Abhimukth Chaudhuri <abhimukth.chaudhuri@mail.mcgill.ca>
2019-03-03 11:29:39 -0500

    Fixed JLabel errors

--- src/ca/mcgill/ecse223/block/view/EditBlocksPage.java
+++ src/ca/mcgill/ecse223/block/view/EditBlocksPage.java
@@ -20,6 +20,7 @@ import java.awt.SystemColor;
 import javax.swing.JComboBox;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class EditBlocksPage {
 
@@ -65,6 +66,7 @@ public class EditBlocksPage {
 		btnCreateNewBlock.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				//TODO: Implement action to add new block to the game
+				frmGameBlockSettings.setVisible(false);
 			}
 		});
 		
@@ -82,20 +84,17 @@ public class EditBlocksPage {
 			}
 		});
 		
-		JFormattedTextField frmtdtxtfldBlockSettings = new JFormattedTextField();
+		JLabel frmtdtxtfldBlockSettings = new JLabel();
 		frmtdtxtfldBlockSettings.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		frmtdtxtfldBlockSettings.setEditable(false);
-		frmtdtxtfldBlockSettings.setText("Block Settings:");
+		frmtdtxtfldBlockSettings.setText("Add a new block to the game:");
 		
 		JSeparator separator = new JSeparator();
 		
-		JFormattedTextField frmtdtxtfldGameBlockList = new JFormattedTextField();
+		JLabel frmtdtxtfldGameBlockList = new JLabel();
 		frmtdtxtfldGameBlockList.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		frmtdtxtfldGameBlockList.setEditable(false);
 		frmtdtxtfldGameBlockList.setText("Game Block List:");
 		
-		JFormattedTextField frmtdtxtfldpleaseSelectA = new JFormattedTextField();
-		frmtdtxtfldpleaseSelectA.setEditable(false);
+		JLabel frmtdtxtfldpleaseSelectA = new JLabel();
 		frmtdtxtfldpleaseSelectA.setBackground(SystemColor.activeCaptionBorder);
 		frmtdtxtfldpleaseSelectA.setHorizontalAlignment(SwingConstants.CENTER);
 		frmtdtxtfldpleaseSelectA.setFont(new Font("Tahoma", Font.PLAIN, 11));
@@ -104,36 +103,37 @@ public class EditBlocksPage {
 		JComboBox comboBox = new JComboBox();
 		GroupLayout groupLayout = new GroupLayout(frmGameBlockSettings.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
+			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addGroup(groupLayout.createSequentialGroup()
 									.addComponent(btnBack)
-									.addPreferredGap(ComponentPlacement.RELATED, 283, Short.MAX_VALUE)
+									.addPreferredGap(ComponentPlacement.RELATED, 280, Short.MAX_VALUE)
 									.addComponent(btnLogout))
 								.addGroup(groupLayout.createSequentialGroup()
 									.addComponent(separator, GroupLayout.PREFERRED_SIZE, 1, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(btnUpdateBlock, GroupLayout.DEFAULT_SIZE, 185, Short.MAX_VALUE)
-										.addComponent(btnCreateNewBlock, GroupLayout.DEFAULT_SIZE, 423, Short.MAX_VALUE)
-										.addComponent(btnDeleteBlock, GroupLayout.DEFAULT_SIZE, 185, Short.MAX_VALUE))))
-							.addContainerGap())
+									.addGap(78)
+									.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 269, GroupLayout.PREFERRED_SIZE))))
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(frmtdtxtfldGameBlockList, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap(28, Short.MAX_VALUE)
+							.addComponent(frmtdtxtfldGameBlockList)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(frmtdtxtfldpleaseSelectA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addContainerGap(17, Short.MAX_VALUE))
-						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-							.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 269, GroupLayout.PREFERRED_SIZE)
-							.addGap(94))))
-				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-					.addGap(167)
-					.addComponent(frmtdtxtfldBlockSettings, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(178, Short.MAX_VALUE))
+							.addComponent(frmtdtxtfldpleaseSelectA))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(25)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(frmtdtxtfldBlockSettings)
+								.addComponent(btnCreateNewBlock, GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE))))
+					.addContainerGap())
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnDeleteBlock, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE)
+						.addComponent(btnUpdateBlock, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE))
+					.addGap(25))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -142,24 +142,23 @@ public class EditBlocksPage {
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnBack)
 						.addComponent(btnLogout))
+					.addGap(16)
+					.addComponent(frmtdtxtfldBlockSettings)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnCreateNewBlock)
 					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(frmtdtxtfldGameBlockList, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(frmtdtxtfldpleaseSelectA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(11)
-					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(14)
-					.addComponent(frmtdtxtfldBlockSettings, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
+						.addComponent(frmtdtxtfldGameBlockList)
+						.addComponent(frmtdtxtfldpleaseSelectA))
+					.addGap(17)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(btnCreateNewBlock)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(btnUpdateBlock)))
+						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnUpdateBlock)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(btnDeleteBlock)
-					.addContainerGap(110, Short.MAX_VALUE))
+					.addContainerGap(40, Short.MAX_VALUE))
 		);
 		frmGameBlockSettings.getContentPane().setLayout(groupLayout);
 	}
